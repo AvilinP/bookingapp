0aed6067372d4472cf7011f943ab5d1a4b547de6
Avilin <avilin.persson@gmail.com>
2021-04-29 16:27:34 +0200

    updated menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -4,31 +4,22 @@ import { Link } from "react-router-dom";
 
 function Menu() {
     return (
-        <nav className="bg-gray-200">
-           
-                <div className="relative flex items-center justify-between h-20">
+        <nav className="bg-gray-200 h-auto flex flex-wrap flex-row justify-center space-x-2 sm:space-x-12 items-center flex-1">
 
-                    <div className="flex-1 flex items-center justify-center sm:items-stretch sm:center">
 
-                        <div className="sm:block sm:ml-6">
+            <Link to="/" className="menu-links">About Us</Link>
 
-                            <div className="flex space-x-4">
-                                <Link to="/"> <img className="menu-logo" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg" alt="Logo"/></Link>
+            <Link to="/card" className="menu-links">Appointments</Link>
 
-                                <Link to="/" className="menu-links">About Us</Link>
+            <Link to="/"> <img className="menu-logo" src="/assets/Dazed Studios.png" alt="Logo" /></Link>
 
-                                <Link to="/card" className="menu-links">Appointments</Link>
+            <Link to="/mybookings" className="menu-links">My bookings</Link>
 
-                                <Link to="/mybookings" className="menu-links">My bookings</Link>
+            <Link to="/login" className="menu-links">Login</Link>
 
-                                <Link to="/login" className="menu-links absolute right-8">Login</Link>
+            <Link to="/search" className="menu-links">Inspiration</Link>
 
-                                <Link to="/search" className="menu-links absolute right-24">Inspiration</Link>
 
-                            </div>
-                        </div>
-                </div>
-            </div>
         </nav>
 
 
