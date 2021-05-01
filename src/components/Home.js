310c58b53b76da51baaa4b420a89cc59f722436e
Avilin <avilin.persson@gmail.com>
2021-05-01 18:29:56 +0200

    sticky nav

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,64 +1,60 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 
 function Home() {
-    
+
     return (
         <>
 
             <div className="min-h-screen ml-1 mr-1 pb-10 gap-1 grid grid-cols-1 grid-rows-1 md:grid-cols-3 md:grid-rows-3 md:min-h-screen">
 
-                <div className="border-2 border-black flex flex-col items-center">
-                    <div> HEJ!</div>
-                    <img src="/assets/Dazed Studios.png" alt="Home page product 1" />
-                    <button className="border-2 border-black m-2 p-2">Click here</button>
+                <div className="border-2 border-black flex flex-col">
+                    {/* <div className="flex justify-center p-2"> bag. </div> */}
+                    <img src="/assets/banana.jpeg" alt="Home page product 1" />
+                    {/* <button className="border-2 border-black m-2 p-2">See more</button> */}
+
                 </div>
 
                 <div className="border-2 border-black flex flex-col">
-                    <div> HEJ!</div>
-                    <img src="/assets/Dazed Studios.png" alt="Home page product 1" />
-                    <button className="border-2 border-black m-2 p-2">Click here</button>
+                    {/* <div className="flex justify-center p-2"> banana. </div> */}
+                    <img src="/assets/banana.jpeg" alt="Home page product 1" />
+                    {/* <button className="border-2 border-black m-2 p-2">See more</button> */}
                 </div>
 
-                <div className="border-2 border-black">
-                    <div> HEJ!</div>
-                    <img src="/assets/Dazed Studios.png" alt="Home page product 1" />
-                    <button className="border-2 border-black m-2 p-2">Click here</button>
+                <div className="border-2 border-black flex flex-col">
+                    {/* <div className="flex justify-center p-2"> pineapple.</div> */}
+                    <img src="/assets/strawberry.jpeg" alt="Home page product 1" />
+                    {/* <button className="border-2 border-black m-2 p-2">See more</button> */}
                 </div>
 
-                <div className="border-2 border-black">
-                    <div> HEJ!</div>
-                    <img src="/assets/Dazed Studios.png" alt="Home page product 1" />
-                    <button className="border-2 border-black m-2 p-2">Click here</button>
+                <div className="border-2 border-black flex flex-col">
+                    <img src="/assets/banana.jpeg" alt="Home page product 1" />
                 </div>
 
-                <div className="border-2 border-black">
-                    <div> HEJ!</div>
-                    <img src="/assets/Dazed Studios.png" alt="Home page product 1" />
-                    <button className="border-2 border-black m-2 p-2">Click here</button>
+                <div className="border-2 border-black flex flex-col">
+                    <img src="/assets/banana.jpeg" alt="Home page product 1" />
                 </div>
 
-                <div className="border-2 border-black">
-                    <div> HEJ!</div>
-                    <img src="/assets/Dazed Studios.png" alt="Home page product 1" />
-                    <button className="border-2 border-black m-2 p-2">Click here</button>
+                <div className="border-2 border-black flex flex-col">
+                    <img src="/assets/banana.jpeg" alt="Home page product 1" />
                 </div>
 
-                <div className="border-2 border-black">
-                    <div> HEJ!</div>
-                    <img src="/assets/Dazed Studios.png" alt="Home page product 1" />
-                    <button className="border-2 border-black m-2 p-2">Click here</button>
+
+                <div className="border-2 border-black flex flex-col">
+                    {/* <div className="flex justify-center p-2"> pineapple.</div> */}
+                    <img src="/assets/strawberry.jpeg" alt="Home page product 1" />
+                    {/* <button className="border-2 border-black m-2 p-2">See more</button> */}
                 </div>
 
-                <div className="border-2 border-black">
-                    <div> HEJ!</div>
-                    <img src="/assets/Dazed Studios.png" alt="Home page product 1" />
-                    <button className="border-2 border-black m-2 p-2">Click here</button>
+                <div className="border-2 border-black flex flex-col">
+                    {/* <div className="flex justify-center p-2"> pineapple.</div> */}
+                    <img src="/assets/strawberry.jpeg" alt="Home page product 1" />
+                    {/* <button className="border-2 border-black m-2 p-2">See more</button> */}
                 </div>
 
-                <div className="border-2 border-black">
-                    <div> HEJ!</div>
-                    <img src="/assets/Dazed Studios.png" alt="Home page product 1" />
-                    <button className="border-2 border-black m-2 p-2">Click here</button>
+                <div className="border-2 border-black flex flex-col">
+                    {/* <div className="flex justify-center p-2"> pineapple.</div> */}
+                    <img src="/assets/strawberry.jpeg" alt="Home page product 1" />
+                    {/* <button className="border-2 border-black m-2 p-2">See more</button> */}
                 </div>
 
 
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -4,27 +4,26 @@ import { Link } from "react-router-dom";
 
 function Menu() {
     return (
-        <nav className="h-auto flex flex-wrap flex-row justify-center space-x-2 sm:space-x-12 items-center flex-1">
+
+        <div className="sticky top-0">
+            <nav className="h-auto flex flex-wrap flex-row justify-center space-x-2 sm:space-x-12 items-center flex-1">
+
+                <Link to="/" className="menu-links absolute left-8">shop.</Link>
+
+                <Link to="/card" className="menu-links absolute left-16">news.</Link>
+
+                <Link to="/"> <img className="menu-logo" src="/assets/Dazed Studios.png" alt="Logo" /></Link>
+
+                <Link to="/mybookings" className="menu-links absolute right-52">cart.</Link>
+
+                <Link to="/login" className="menu-links absolute right-8">login.</Link>
+
+                <Link to="/search" className="menu-links absolute right-24">inspiration.</Link>
 
 
-            <Link to="/" className="menu-links absolute left-8">Shop</Link>
+            </nav>
 
-            <Link to="/card" className="menu-links absolute left-16">New Arrivals</Link>
-
-            <Link to="/"> <img className="menu-logo" src="/assets/Dazed Studios.png" alt="Logo" /></Link>
-
-            <Link to="/mybookings" className="menu-links absolute right-52">Cart</Link>
-
-            <Link to="/login" className="menu-links absolute right-8">Login</Link>
-
-            <Link to="/search" className="menu-links absolute right-24">Inspiration</Link>
-
-
-        </nav>
-
-
-
-
+        </div>
 
     )
 }
